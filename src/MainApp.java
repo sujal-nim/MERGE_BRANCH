4ec4a1bd9ca75968d937c4f5fc586462afd871b1
sujal-nim <sujalnimbalkar10@gmail.com>
2026-01-12 10:15:37 +0530

    Added basic Java application in main branch

---
+++ src/MainApp.java
@@ -0,0 +1,8 @@
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Main Branch Application");
+        System.out.println("Version: 1.0");
+    }
+}
+
