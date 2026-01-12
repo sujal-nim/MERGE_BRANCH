775ddf37ab281dffb827d8bc4c5e4ca19138762e
sujal-nim <sujalnimbalkar10@gmail.com>
2026-01-12 10:22:57 +0530

    Added login feature in feature branch

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,6 +3,13 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Main Branch Application");
         System.out.println("Version: 1.0");
+
+        loginFeature();
+    }
+
+    public static void loginFeature() {
+        System.out.println("Login Feature Added");
+        System.out.println("Status: Feature Branch");
     }
 }
 
